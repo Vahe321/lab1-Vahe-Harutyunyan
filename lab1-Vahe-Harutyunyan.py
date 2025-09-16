c72fdc10fc04e23cac2c3d77e72a460e9a5ad4c2
Vahe321 <vaheharu811@gmail.com>
2025-09-16 15:01:46 +0300

    Add files via upload

---
+++ lab1-Vahe-Harutyunyan.py
@@ -0,0 +1,12 @@
+def in_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+numbers = [4, 5, 1, 7, 3, 9]
+print(in_sort(numbers))
